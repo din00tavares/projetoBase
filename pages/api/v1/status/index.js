43f291d5a27acc5747260037745e4484ac75ac64
Dino Tavares <31374010+din00tavares@users.noreply.github.com>
2024-01-08 17:40:22 +0000

    Criação do endpoint `api\v1\status`

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,7 @@
+function status(request, response) {
+  response.status(200).json({
+    alunos: "são acima da média",
+  });
+}
+
+export default status;
--- tests/code.test.js
+++
@@ -1,6 +0,0 @@
-const code = require("../models/code.js");
-
-test("Sum 40 + 2 should be 42", () => {
-  const result = code.sumCode(40, 2);
-  expect(result).toBe(42);
-});
